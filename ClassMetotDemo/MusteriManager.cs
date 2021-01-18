8540f6e8e5f81ee9b5ff1ba098fab831905f6410
Gülnihal Coşkun <glnhcsl.22@gmail.com>
2021-01-18 23:29:04 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi!:"+ musteri.Adi+" "+musteri.SoyAdi+"("+musteri.Id+")");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi!:"+ musteri.Adi + " " + musteri.SoyAdi + "(" + musteri.Id + ")");
+        }
+        public void Sırala(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri sıralandı!:" + musteri.Adi + " " + musteri.SoyAdi + "(" + musteri.Id + ")");
+        }
+
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Sıla";
+            musteri1.SoyAdi = "Demir";
+            musteri1.Id = 303030;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ayşe";
+            musteri2.SoyAdi = "Işık";
+            musteri2.Id = 606060;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Sevinç";
+            musteri3.SoyAdi = "Aydın";
+            musteri3.Id = 505050;
+            
+            Musteri[] musteriler= new Musteri [] { musteri1,musteri2,musteri3};
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.SoyAdi);
+                Console.WriteLine(musteri.Id);
+            }
+            Console.WriteLine("---------------");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Sırala(musteri3);
+
+        }
+    }
+}
